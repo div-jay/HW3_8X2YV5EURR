7a8a60b2012a701263e62fc107c9061dfa347b7b
div-jay <68689023+div-jay@users.noreply.github.com>
2020-08-13 17:12:33 -0700

    my react assignment-no.3

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import "./../css/components.css";
+import "./../css/global.css";
+import "./../css/layout.css";
+import "./../css/sample-wallpaper.css";
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <nav className="navbar">
+                        <div className="container">
+                            <a href="#">Hulk</a>
+                            <a href="#" className="logo is-active">Mavel</a>
+                            <a href="#">Venom</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+export default Navbar
